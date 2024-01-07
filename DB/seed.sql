8c4bb4ea5621c5048bdac781d19a1ec806666ba9
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-06 20:29:23 -0500

    Update seed.sql

--- DB/seed.sql
+++ DB/seed.sql
@@ -4,30 +4,26 @@ INSERT INTO department
     (name)
 VALUES
     ('Sales'),
-    ('Engineering'),
+    ('Floor Worker'),
     ('Finance'),
     ('Legal');
 
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
-    ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
+    ('Salesperson', 60000, 1),
+    ('Floor Worker', 50000, 2),
+    ('Accountant', 75000, 3),
+    ('Legal Team ', 80000, 4),
 
 INSERT INTO employee
-    (first_name, last_name, role_id, manager_id)
+    (first_name, last_name, role_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+    ('John', 'Doe', 1),
+    ('Mike', 'Chan', 2),
+    ('Ashley', 'Rodriguez', 3),
+    ('Kevin', 'Tupik', 4),
+    ('Kunal', 'Singh', 5),
+    ('Malia', 'Brown', 6),
+    ('Sarah', 'Lourd', 7),
+    ('Tom', 'Allen', 8);
